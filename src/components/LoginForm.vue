4da74fc9c68700845d6a5c46b7f65cbaf7926029
Atsushi <>
2019-11-24 01:45:57 +0900

    login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -31,7 +31,7 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import { mapState, mapGetters, mapActions } from "vuex";
 
 export default {
   name: "login-form",
@@ -47,6 +47,16 @@ export default {
     },
     ...mapState(["loading", "error"]),
     ...mapGetters(["hasError"])
+  },
+  methods: {
+    ...mapActions(["login"]),
+    async onSubmit() {
+      const result = await this.login(this.userId);
+      console.log(result);
+      if (result) {
+        this.$router.push("chat");
+      }
+    }
   }
 };
 </script>
